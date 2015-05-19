038d9e162697117d0007265e4e26cbdb81af89d2
ChelseaBurns <co.designux@gmail.com>
2015-05-19 15:12:45 -0500

    updated bowcat in build.sh file

--- build.sh
+++ build.sh
@@ -8,37 +8,26 @@ rm -rf public
 cp -r src public
 
 # compile jade to html
-./node_modules/.bin/nodemon -e jade --watch src --exec "
-  ./node_modules/.bin/jade src -o public -PH
-  rm -rf src/*.html public/_partials" &
+./node_modules/.bin/jade src -o public -PH
+rm -rf public/_partials
 
 # compile sass to css
 ./node_modules/.bin/node-sass \
   --output-style compressed \
   --source-map-embed \
-  --recursive \
-  src/_styles/main.scss public/css/main.css &
+  src/_styles/main.scss public/css/main.css
 
 # convert ES6 JS to ES5
-./node_modules/.bin/babel \
-  src \
-  --out-dir public \
-  -s inline \
-  -w &
+./node_modules/.bin/babel src --out-dir public -s inline
 
 # concat bower_components to public/lib directory
 if [ -d "bower_components" ]; then
-  ./node_modules/.bin/bowcat . -o public/lib -m
+  ./node_modules/.bin/bowcat . -o public/lib
 fi
 
-echo "clean"
 # clean unneeded files
 rm -rf public/_styles \
        public/*.jade \
        public/**/*.jade \
        public/*.scss \
        public/**/*.scss
-
-echo "╔═══════════════════════════════════════════╗"
-echo "║          Watching for changes...          ║"
-echo "╚═══════════════════════════════════════════╝"
